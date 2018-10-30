cce2d8e65c80246952f83b8da61d2b9c49b73c93
Elise Wong <eyw009@ucsd.edu>
2018-10-30 00:01:59 -0700

    Created skeleton for cart and outfit 1

--- home.js
+++ home.js
@@ -3,6 +3,19 @@ $(document).ready(function() {
 })
 
 function initializePage() {
+  $("#item1").click(function(e) {
+    $(location).attr("href", "outfit1.html");
+  });
 
+  $(".backhome").click(function(e) {
+    $(location).attr("href", "home.html");
+  });
 
-};
+  $(".cart").click(function(e) {
+    $(location).attr("href", "cart.html");
+  });
+/*
+  function toOutfit() {
+    $(location).attr('href',"outfit1.html");
+  }*/
+}
