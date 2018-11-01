0101a50cd194ac9e684c73f9ff69b5f6c03d7243
Elise Wong <eyw009@ucsd.edu>
2018-11-01 12:53:04 -0700

    created outfit1 boxes

--- home.js
+++ home.js
@@ -3,19 +3,26 @@ $(document).ready(function() {
 })
 
 function initializePage() {
-  $("#item1").click(function(e) {
-    $(location).attr("href", "outfit1.html");
-  });
-
+  //Press back button while at an outfit
   $(".backhome").click(function(e) {
     $(location).attr("href", "index.html");
   });
 
+  //Press cart button
   $(".cart").click(function(e) {
     $(location).attr("href", "cart.html");
   });
-/*
-  function toOutfit() {
-    $(location).attr('href',"outfit1.html");
-  }*/
+
+  //Go to outfit1
+  $("#item1").click(function(e) {
+    $(location).attr("href", "outfit1.html");
+  });
+
+  //Popup for outfit1 item1
+  $(".popup").click(myFunction);
+  function myFunction(event) {
+      var popup = document.getElementById("myPopup");
+      popup.classList.toggle("show");
+  }
+
 }
