58c09784818b27a99a9e1a87c9acb841511a87d0
Elise Wong <eyw009@ucsd.edu>
2018-11-06 11:24:18 -0800

    added navbars to main page

--- home.js
+++ home.js
@@ -1,8 +1,30 @@
+var outfitData = [
+  {'outfitpic': 'outfit1.jpeg', 'index': 1},
+  {'outfitpic': 'outfit1.jpeg', 'index': 2},
+  {'outfitpic': 'outfit1.jpeg', 'index': 3},
+  {'outfitpic': 'outfit1.jpeg', 'index': 4},
+  {'outfitpic': 'outfit1.jpeg', 'index': 5},
+  {'outfitpic': 'outfit1.jpeg', 'index': 6}
+]
+
+
 $(document).ready(function() {
   initializePage();
 })
 
 function initializePage() {
+  /* Set the width of the side navigation to 250px */
+  $(".opennav").click(openNav);
+  function openNav(event) {
+      document.getElementById("mySidenav").style.width = "250px";
+  }
+
+  /* Set the width of the side navigation to 0 */
+  $(".closenav").click(closeNav);
+  function closeNav(event) {
+      document.getElementById("mySidenav").style.width = "0";
+  }
+
   //Press back button while at an outfit
   $(".backhome").click(function(e) {
     $(location).attr("href", "index.html");
@@ -39,6 +61,17 @@ function initializePage() {
   });
 
 
+  var source   = $("#outfit-template").html();
+  var template = Handlebars.compile(source);
+
+  var parentDiv = $("#templatedOutfits");
+
+  // now iterate through the complexData list and keep appending:
+  for (var i = 0; i < outfitData.length; i++) {
+    var curData = outfitData[i];
+    var curHtml = template(curData);
+    parentDiv.append(curHtml);
+  };
 
 
 
