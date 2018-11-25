ae47fe280421a9f0b5c59f34e0563cd990ca08dc
Elise Wong <eyw009@ucsd.edu>
2018-11-25 11:31:40 -0800

    Added more filters

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 var outfitData = [
-  {'outfitpic': 'https://images.urbanoutfitters.com/is/image/UrbanOutfitters/45466208_018_b?$xlarge$&amp;hei=900&amp;qlt=80&amp;fit=constrain', 'index': 1, 'Season': 'Spring'},
-  {'outfitpic': 'https://images.urbanoutfitters.com/is/image/UrbanOutfitters/47310701_026_b?$xlarge$&amp;hei=900&amp;qlt=80&amp;fit=constrain', 'index': 2, 'Season': 'Summer'},
-  {'outfitpic': 'https://images.urbanoutfitters.com/is/image/UrbanOutfitters/43306216_024_b?$redesign-zoom-5x$&amp;hei=2175&amp;qlt=80&amp;fit=constrain', 'index': 3, 'Season': 'Winter'},
-  {'outfitpic': 'https://images.urbanoutfitters.com/is/image/UrbanOutfitters/48249775_053_b?$xlarge$&amp;hei=900&amp;qlt=80&amp;fit=constrain', 'index': 4, 'Season': 'Summer'},
-  {'outfitpic': 'https://images.urbanoutfitters.com/is/image/UrbanOutfitters/43113000_015_b?$xlarge$&amp;hei=900&amp;qlt=80&amp;fit=constrain', 'index': 5, 'Season': 'Fall'},
-  {'outfitpic': 'https://images.urbanoutfitters.com/is/image/UrbanOutfitters/47941026_031_b?$xlarge$&amp;hei=900&amp;qlt=80&amp;fit=constrain', 'index': 6, 'Season': 'Fall'}
+  {'outfitpic': 'https://images.urbanoutfitters.com/is/image/UrbanOutfitters/45466208_018_b?$xlarge$&amp;hei=900&amp;qlt=80&amp;fit=constrain', 'index': 1, 'Season': 'Spring', 'Style': 'Casual'},
+  {'outfitpic': 'https://images.urbanoutfitters.com/is/image/UrbanOutfitters/47310701_026_b?$xlarge$&amp;hei=900&amp;qlt=80&amp;fit=constrain', 'index': 2, 'Season': 'Summer', 'Style': 'Casual'},
+  {'outfitpic': 'https://images.urbanoutfitters.com/is/image/UrbanOutfitters/43306216_024_b?$redesign-zoom-5x$&amp;hei=2175&amp;qlt=80&amp;fit=constrain', 'index': 3, 'Season': 'Winter', 'Style': 'Casual'},
+  {'outfitpic': 'https://images.urbanoutfitters.com/is/image/UrbanOutfitters/48249775_053_b?$xlarge$&amp;hei=900&amp;qlt=80&amp;fit=constrain', 'index': 4, 'Season': 'Summer', 'Style': 'Pastel'},
+  {'outfitpic': 'https://images.urbanoutfitters.com/is/image/UrbanOutfitters/43113000_015_b?$xlarge$&amp;hei=900&amp;qlt=80&amp;fit=constrain', 'index': 5, 'Season': 'Fall', 'Style': 'Chic'},
+  {'outfitpic': 'https://images.urbanoutfitters.com/is/image/UrbanOutfitters/47941026_031_b?$xlarge$&amp;hei=900&amp;qlt=80&amp;fit=constrain', 'index': 6, 'Season': 'Fall', 'Style': 'Vintage'}
 ]
 
+/* Set acceptable usernames and passwords for login*/
 var userpass1 = {'username':'iamuser1', 'password':'170'};
 var userpass2 = {'username':'iamuser2', 'password':'120'};
 
 localStorage.setItem('userpass1', JSON.stringify(userpass1));
 localStorage.setItem('userpass2', JSON.stringify(userpass2));
+
 
 $(document).ready(function() {
   initializePage();
@@ -19,6 +21,7 @@ $(document).ready(function() {
 
 function initializePage() {
 
+  /*-------------------LOGIN----------------*/
   $("#loginchange").click(function(e) {
     /*Check if username and password are correct*/
     var userin = $('#username').val();
@@ -41,7 +44,6 @@ function initializePage() {
       console.log('alert');
       alert("Incorrect Username or Password!")
     }
-
   });
 
   $("#loggedout").click(function(e) {
@@ -60,96 +62,212 @@ function initializePage() {
     $('#loggedin').attr("href", "login.html");
   }
 
-  localStorage.setItem('yes1', 0);
-  localStorage.setItem('yes2', 0);
-  localStorage.setItem('yes3', 0);
+  /*-------------------SEASON FILTERS-------------------*/
+  var name = window.localStorage.getItem('currFilter');
 
+  $("#Spring").click(function(e){
+    $('#currfilter').text('Spring');
+    $("#Spring").html("<span class='hvr-grow' style='color:#fb91b2'>Spring</span>");
+    $("#Fall").html("<span class='hvr-grow'>Fall</span>");
+    $("#Summer").html("<span class='hvr-grow'>Summer</span>");
+    $("#Winter").html("<span class='hvr-grow'>Winter</span>");
+    $("#Pastel").html("<span class='hvr-grow'>Pastel</span>");
+    $("#Casual").html("<span class='hvr-grow'>Casual</span>");
+    $("#Chic").html("<span class='hvr-grow'>Chic</span>");
+    $("#Vintage").html("<span class='hvr-grow'>Vintage</span>");
 
-  /*-------------------FILTERS-------------------*/
-// to get this to work like in class, comment out the "STEP 1" parts
-// above between BEGIN and END.
-var source   = $("#outfit-template").html();
-var template = Handlebars.compile(source);
-
-var parentDiv = $("#templatedOutfits");
-
-var name = window.localStorage.getItem('currFilter');
-
-$("#Spring").click(function(e){
-
-  $('#currfilter').text('Spring');
-
-  for (var i = 0; i < outfitData.length; i++) {
-    var curData = outfitData[i];
-    var outfitnum = "#outfit" + (i+1);
-    if (curData.Season != 'Spring') {
-      $(outfitnum).hide();
+    for (var i = 0; i < outfitData.length; i++) {
+      var curData = outfitData[i];
+      var outfitnum = "#outfit" + (i+1);
+      if (curData.Season != 'Spring') {
+        $(outfitnum).hide();
+      }
+      else{
+        $(outfitnum).show();
+      }
     }
-    else{
+  });
+
+  $("#Summer").click(function(e){
+    $('#currfilter').text('Summer');
+    $("#Summer").html("<span class='hvr-grow' style='color:#fb91b2'>Summer</span>");
+    $("#Spring").html("<span class='hvr-grow'>Spring</span>");
+    $("#Fall").html("<span class='hvr-grow'>Fall</span>");
+    $("#Winter").html("<span class='hvr-grow'>Winter</span>");
+    $("#Pastel").html("<span class='hvr-grow'>Pastel</span>");
+    $("#Casual").html("<span class='hvr-grow'>Casual</span>");
+    $("#Chic").html("<span class='hvr-grow'>Chic</span>");
+    $("#Vintage").html("<span class='hvr-grow'>Vintage</span>");
+
+    for (var i = 0; i < outfitData.length; i++) {
+      var curData = outfitData[i];
+      var outfitnum = "#outfit" + (i+1);
+      if (curData.Season != 'Summer') {
+        $(outfitnum).hide();
+      }
+      else{
+        $(outfitnum).show();
+      }
+    }
+  });
+
+  $("#Fall").click(function(e){
+    $('#currfilter').text('Fall');
+    $("#Fall").html("<span class='hvr-grow' style='color:#fb91b2'>Fall</span>");
+    $("#Spring").html("<span class='hvr-grow'>Spring</span>");
+    $("#Summer").html("<span class='hvr-grow'>Summer</span>");
+    $("#Winter").html("<span class='hvr-grow'>Winter</span>");
+    $("#Pastel").html("<span class='hvr-grow'>Pastel</span>");
+    $("#Casual").html("<span class='hvr-grow'>Casual</span>");
+    $("#Chic").html("<span class='hvr-grow'>Chic</span>");
+    $("#Vintage").html("<span class='hvr-grow'>Vintage</span>");
+
+    for (var i = 0; i < outfitData.length; i++) {
+      var curData = outfitData[i];
+      var outfitnum = "#outfit" + (i+1);
+      if (curData.Season != 'Fall') {
+        $(outfitnum).hide();
+      }
+      else{
+        $(outfitnum).show();
+      }
+    }
+  });
+
+  $("#Winter").click(function(e){
+    $('#currfilter').text('Winter');
+    $("#Winter").html("<span class='hvr-grow' style='color:#fb91b2'>Winter</span>");
+    $("#Spring").html("<span class='hvr-grow'>Spring</span>");
+    $("#Summer").html("<span class='hvr-grow'>Summer</span>");
+    $("#Fall").html("<span class='hvr-grow'>Fall</span>");
+    $("#Pastel").html("<span class='hvr-grow'>Pastel</span>");
+    $("#Casual").html("<span class='hvr-grow'>Casual</span>");
+    $("#Chic").html("<span class='hvr-grow'>Chic</span>");
+    $("#Vintage").html("<span class='hvr-grow'>Vintage</span>");
+
+    for (var i = 0; i < outfitData.length; i++) {
+      var curData = outfitData[i];
+      var outfitnum = "#outfit" + (i+1);
+      if (curData.Season != 'Winter') {
+        $(outfitnum).hide();
+      }
+      else{
+        $(outfitnum).show();
+      }
+    }
+  });
+
+  /*-------------------STYLE FILTERS-------------------*/
+  $("#Pastel").click(function(e){
+    $('#currfilter').text('Pastel');
+    $("#Pastel").html("<span class='hvr-grow' style='color:#fb91b2'>Pastel</span>");
+    $("#Casual").html("<span class='hvr-grow'>Casual</span>");
+    $("#Chic").html("<span class='hvr-grow'>Chic</span>");
+    $("#Vintage").html("<span class='hvr-grow'>Vintage</span>");
+    $("#Spring").html("<span class='hvr-grow'>Spring</span>");
+    $("#Fall").html("<span class='hvr-grow'>Fall</span>");
+    $("#Summer").html("<span class='hvr-grow'>Summer</span>");
+    $("#Winter").html("<span class='hvr-grow'>Winter</span>");
+
+    for (var i = 0; i < outfitData.length; i++) {
+      var curData = outfitData[i];
+      var outfitnum = "#outfit" + (i+1);
+      if (curData.Style != 'Pastel') {
+        $(outfitnum).hide();
+      }
+      else{
+        $(outfitnum).show();
+      }
+    }
+  });
+
+  $("#Casual").click(function(e){
+    $('#currfilter').text('Casual');
+    $("#Casual").html("<span class='hvr-grow' style='color:#fb91b2'>Casual</span>");
+    $("#Pastel").html("<span class='hvr-grow'>Pastel</span>");
+    $("#Chic").html("<span class='hvr-grow'>Chic</span>");
+    $("#Vintage").html("<span class='hvr-grow'>Vintage</span>");
+    $("#Spring").html("<span class='hvr-grow'>Spring</span>");
+    $("#Fall").html("<span class='hvr-grow'>Fall</span>");
+    $("#Summer").html("<span class='hvr-grow'>Summer</span>");
+    $("#Winter").html("<span class='hvr-grow'>Winter</span>");
+
+    for (var i = 0; i < outfitData.length; i++) {
+      var curData = outfitData[i];
+      var outfitnum = "#outfit" + (i+1);
+      if (curData.Style != 'Casual') {
+        $(outfitnum).hide();
+      }
+      else{
+        $(outfitnum).show();
+      }
+    }
+  });
+
+  $("#Chic").click(function(e){
+    $('#currfilter').text('Chic');
+    $("#Chic").html("<span class='hvr-grow' style='color:#fb91b2'>Chic</span>");
+    $("#Casual").html("<span class='hvr-grow'>Casual</span>");
+    $("#Pastel").html("<span class='hvr-grow'>Pastel</span>");
+    $("#Vintage").html("<span class='hvr-grow'>Vintage</span>");
+    $("#Spring").html("<span class='hvr-grow'>Spring</span>");
+    $("#Fall").html("<span class='hvr-grow'>Fall</span>");
+    $("#Summer").html("<span class='hvr-grow'>Summer</span>");
+    $("#Winter").html("<span class='hvr-grow'>Winter</span>");
+
+    for (var i = 0; i < outfitData.length; i++) {
+      var curData = outfitData[i];
+      var outfitnum = "#outfit" + (i+1);
+      if (curData.Style != 'Chic') {
+        $(outfitnum).hide();
+      }
+      else{
+        $(outfitnum).show();
+      }
+    }
+  });
+
+  $("#Vintage").click(function(e){
+    $('#currfilter').text('Vintage');
+    $("#Vintage").html("<span class='hvr-grow' style='color:#fb91b2'>Vintage</span>");
+    $("#Casual").html("<span class='hvr-grow'>Casual</span>");
+    $("#Chic").html("<span class='hvr-grow'>Chic</span>");
+    $("#Pastel").html("<span class='hvr-grow'>Pastel</span>");
+    $("#Spring").html("<span class='hvr-grow'>Spring</span>");
+    $("#Fall").html("<span class='hvr-grow'>Fall</span>");
+    $("#Summer").html("<span class='hvr-grow'>Summer</span>");
+    $("#Winter").html("<span class='hvr-grow'>Winter</span>");
+
+    for (var i = 0; i < outfitData.length; i++) {
+      var curData = outfitData[i];
+      var outfitnum = "#outfit" + (i+1);
+      if (curData.Style != 'Vintage') {
+        $(outfitnum).hide();
+      }
+      else{
+        $(outfitnum).show();
+      }
+    }
+  });
+
+  /*-------------------DELETE FILTERS-------------------*/
+  $(".delfilters").click(function(e){
+    $('#currfilter').text('None');
+    $("#Spring").html("<span class='hvr-grow'>Spring</span>");
+    $("#Fall").html("<span class='hvr-grow'>Fall</span>");
+    $("#Summer").html("<span class='hvr-grow'>Summer</span>");
+    $("#Winter").html("<span class='hvr-grow'>Winter</span>");
+    $("#Pastel").html("<span class='hvr-grow'>Pastel</span>");
+    $("#Casual").html("<span class='hvr-grow'>Casual</span>");
+    $("#Chic").html("<span class='hvr-grow'>Chic</span>");
+    $("#Vintage").html("<span class='hvr-grow'>Vintage</span>");
+
+    for (var i = 0; i < outfitData.length; i++) {
+      var curData = outfitData[i];
+      var outfitnum = "#outfit" + (i+1);
       $(outfitnum).show();
     }
-  }
-});
-
-$("#Summer").click(function(e){
-
-  $('#currfilter').text('Summer');
-
-  for (var i = 0; i < outfitData.length; i++) {
-    var curData = outfitData[i];
-    var outfitnum = "#outfit" + (i+1);
-    if (curData.Season != 'Summer') {
-      $(outfitnum).hide();
-    }
-    else{
-      $(outfitnum).show();
-    }
-  }
-});
-
-$("#Fall").click(function(e){
-
-  $('#currfilter').text('Fall');
-
-  for (var i = 0; i < outfitData.length; i++) {
-    var curData = outfitData[i];
-    var outfitnum = "#outfit" + (i+1);
-    if (curData.Season != 'Fall') {
-      $(outfitnum).hide();
-    }
-    else{
-      $(outfitnum).show();
-    }
-  }
-});
-
-$("#Winter").click(function(e){
-
-  $('#currfilter').text('Winter');
-
-  for (var i = 0; i < outfitData.length; i++) {
-    var curData = outfitData[i];
-    var outfitnum = "#outfit" + (i+1);
-    if (curData.Season != 'Winter') {
-      $(outfitnum).hide();
-    }
-    else{
-      $(outfitnum).show();
-    }
-  }
-});
-
-
-$(".delfilters").click(function(e){
-
-  $('#currfilter').text('None');
-
-  for (var i = 0; i < outfitData.length; i++) {
-    var curData = outfitData[i];
-    var outfitnum = "#outfit" + (i+1);
-    $(outfitnum).show();
-  }
-});
+  });
 
   /*-------------------BUTTON PRESSES-------------------*/
   //Press back button while at an outfit
@@ -158,6 +276,9 @@ $(".delfilters").click(function(e){
   });
 
   /*-------------------OUTFIT TEMPLATE-------------------*/
+  localStorage.setItem('yes1', 0);
+  localStorage.setItem('yes2', 0);
+  localStorage.setItem('yes3', 0);
 
   var source   = $("#outfit-template").html();
   var template = Handlebars.compile(source);
@@ -165,7 +286,7 @@ $(".delfilters").click(function(e){
   var parentDiv = $("#templatedOutfits");
 
   // now iterate through the complexData list and keep appending:
- for (var i = 0; i < outfitData.length; i++) {
+  for (var i = 0; i < outfitData.length; i++) {
     var curData = outfitData[i];
     var curHtml = template(curData);
     parentDiv.append(curHtml);
